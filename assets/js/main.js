4532f4954a299adac127be050ab5dafd3fc0f646
Mika <mika.devs100@gmail.com>
2025-06-14 18:06:41 -0300

    portfolio website

---
+++ assets/js/main.js
@@ -0,0 +1,92 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
+
+
+/*=============== MENU SHOW ===============*/
+/* Validate if constant exists */
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*=============== MENU HIDDEN ===============*/
+/* Validate if constant exists */
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () => {
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header') 
+}
+window.addEventListener('scroll', bgHeader)
+
+/*=============== SHOW/HIDE HEADER ON SCROLL ===============
+let lastScrollTop = 0;
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', function() {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
+        // Scroll Down
+        header.style.transform = 'translateY(-100%)'; // Hide the header
+    } else {
+        // Scroll Up
+        header.style.transform = 'translateY(0)'; // Show the header
+    }
+    
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Avoid negative values
+});
+*/
+
+/*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the light-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the lights / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+/*=============== FAQ OPEN ONE ===============*/ 
+document.querySelectorAll('details').forEach(detail => {
+    detail.addEventListener('click', function() {
+        // Fecha todos os outros details, exceto o que foi clicado
+        document.querySelectorAll('details').forEach(otherDetail => {
+            if (otherDetail !== this) {
+                otherDetail.removeAttribute('open');
+            }
+        });
+    });
+});
